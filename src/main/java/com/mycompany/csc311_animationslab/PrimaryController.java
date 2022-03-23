d587454731f93ff6744a47cc03391a70b07692ee
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-03-23 09:51:10 -0400

    Added Yellow Transition

--- src/main/java/com/mycompany/csc311_animationslab/PrimaryController.java
+++ src/main/java/com/mycompany/csc311_animationslab/PrimaryController.java
@@ -1,7 +1,11 @@
 package com.mycompany.csc311_animationslab;
 
 import java.io.IOException;
+import javafx.animation.FillTransition;
 import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class PrimaryController {
 
@@ -9,4 +13,19 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private Rectangle blueRect;
+
+    @FXML
+    private void yellowTransition() {
+
+        FillTransition changeToYellow
+                = new FillTransition(Duration.seconds(2), blueRect);
+        changeToYellow.setToValue(Color.YELLOW);        
+        changeToYellow.setCycleCount(1);
+        changeToYellow.play();
+
+    }
+
 }
